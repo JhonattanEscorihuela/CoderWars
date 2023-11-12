ae4b24d12ae153b12d21875cc18d8c225ac3136d
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2023-11-11 21:15:26 -0500

    Ejercicio1

---
+++ Ejercicio1.js
@@ -0,0 +1,24 @@
+
+
+function toCamelCase(str) {
+
+    let split;
+
+    if (str.includes("_") || str.includes("-")) {
+        // Divide por guiones bajos o guiones
+        split = str.split(/[_-]/);
+    }
+
+    if (split && split.length > 1) {
+        let camelCaseString = split.map((elemento, index) => {
+            return index === 0 ? elemento : elemento.charAt(0).toUpperCase() + elemento.slice(1);
+        }).join("");
+
+        return camelCaseString;
+    } else {
+
+        return str;
+    }
+}
+
+console.log(toCamelCase("the_stealth-warrior"));

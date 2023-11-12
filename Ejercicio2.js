80796a0dc64ffafb9a2fc4ff2b9a783e68db86d5
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2023-11-11 21:31:48 -0500

    ejercicio2

---
+++ Ejercicio2.js
@@ -0,0 +1,16 @@
+
+
+let numero = 62563123;
+
+numero = numero.toString().split("");
+
+numero.sort((a, b) => {
+    return b - a
+});
+
+numero = numero.join("");
+console.log(typeof numero);
+
+
+console.log(parseInt(numero));
+

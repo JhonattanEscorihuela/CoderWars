5b3b75736b7a3bc50786fa88c5d10fc5d5f585b2
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2023-11-16 22:44:55 -0500

    digitalRoot

---
+++ Ejercicio13.js
@@ -0,0 +1,19 @@
+function digitalRoot(n) {
+    n = n.toString().split("");
+    let out = 0;
+
+    n.map(e => {
+        out += parseInt(e)
+    });
+
+    let l = out.toString().length;
+
+    if (l > 1) {
+        return digitalRoot(out);
+    } else {
+        return out;
+    }
+
+}
+
+console.log(digitalRoot(942));

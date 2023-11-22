24e54b73411e44907505936179df5541a0e728f9
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2023-11-22 00:20:24 -0500

    generateHastag

--- Ejercicio20.js
+++ Ejercicio20.js
@@ -1,4 +1,12 @@
 function generateHashtag(str) {
+    const words = str.trim().split(" ");
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    const hashtag = `#${capitalizedWords.join("")}`;
+
+    return hashtag.length > 140 || str.trim() === "" ? false : hashtag;
 }
 
+
 console.log(generateHashtag("Do We have A Hashtag"));
+console.log(generateHashtag(""));
+console.log(generateHashtag("code" + " ".repeat(140) + "wars"));
